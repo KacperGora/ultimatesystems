1a8ce34862051dc1a19aea69c99f2370aa484775
KacperGora <kacpergoora@gmail.com>
2022-10-05 11:26:55 +0200

    refactoring files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
 
-import Login from "./components/LoginPage/Login";
-import Register from "./components/Register/Register";
+import Login from "./components/Auth/LoginPage/Login";
+import Register from "./components/Auth/Register/Register";
 import UsersList from "./components/UsersList/UsersList";
+
 
 const App: React.FC = () => (
   <>
