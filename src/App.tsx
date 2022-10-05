20a02b6ce58041a9d3413554f75e2dedf6d26c6b
KacperGora <kacpergoora@gmail.com>
2022-10-05 12:01:07 +0200

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
-
-import Login from "./components/Auth/LoginPage/Login";
-import Register from "./components/Auth/Register/Register";
-import UsersList from "./components/UsersList/UsersList";
 import Routers from "./routers/Routers";
 
 const App: React.FC = () => (
